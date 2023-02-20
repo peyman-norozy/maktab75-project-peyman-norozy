caf992b2b6ab3af87aa3e1b9c01d51540853164f
Peyman Norozi <peyman.norozy@gmail.com>
2023-02-20 00:40:06 -0800

    add the sendNavState as send navigation state( display or not)

--- src/store/cart-actions.jsx
+++ src/store/cart-actions.jsx
@@ -61,3 +61,36 @@ export const sendCartData = (cart) => {
     }
   };
 };
+
+export const sendNavstate = (cart) => {
+  return async (dispatch) => {
+    const sendRequest = async () => {
+      const response = await axios.post("http://localhost:3002/navAuth", {
+        showNavBar: false,
+      });
+
+      if (response.statusText !== "OK") {
+        throw new Error("Sending cart data failed.");
+      }
+    };
+
+    try {
+      await sendRequest();
+      // dispatch(
+      //   uiActions.showNotification({
+      //     status: "success",
+      //     title: "Success!",
+      //     message: "Sent cart data successfully!",
+      //   })
+      // );
+    } catch (error) {
+      // dispatch(
+      //   uiActions.showNotification({
+      //     status: "error",
+      //     title: "Error!",
+      //     message: "Sending cart data failed!",
+      //   })
+      // );
+    }
+  };
+};
