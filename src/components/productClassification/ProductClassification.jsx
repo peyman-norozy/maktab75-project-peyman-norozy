a6339c3e67d722cbba2a79265bafebc66d4d5bd6
Peyman Norozi <peyman.norozy@gmail.com>
2023-02-22 02:58:14 -0800

    add pointer class to the classification bar

--- src/components/productClassification/ProductClassification.jsx
+++ src/components/productClassification/ProductClassification.jsx
@@ -2,8 +2,8 @@ const ProductClassification = () => {
   return (
     <ul className="text-sm flex gap-4">
       <li className="border-l-2 border-zinc-700 pl-2 mb-2">دسته بندی کالاها</li>
-      <li>موبایل</li>
-      <li>ساعت</li>
+      <li className="cursor-pointer">موبایل</li>
+      <li className="cursor-pointer">ساعت</li>
     </ul>
   );
 };
