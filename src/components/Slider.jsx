07a2bab980abd9e3a6a91fbdd51f21b1343b9f57
Peyman Norozi <peyman.norozy@gmail.com>
2023-02-21 23:38:14 -0800

    add the slider component

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,8 +1,28 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+import "../index.css";
+
+// import required modules
+import { Navigation } from "swiper";
+
 const Slider = () => {
   return (
-    <>
-      <div>peyman</div>
-    </>
+    <div className="flex justify-center mt-20">
+      <div className="w-[70%] rounded-2xl overflow-hidden">
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          <SwiperSlide>
+            <img src="./images/slider/bannerApple.png" alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="./images/slider/bannerWatch.png" alt="" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
   );
 };
 
