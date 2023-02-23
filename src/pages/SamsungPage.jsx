8f4ce436eb605f604afa343b3ddac9aa65e82eda
Peyman Norozi <peyman.norozy@gmail.com>
2023-02-23 13:58:12 -0800

    add the pagination to samsungPage component

--- src/pages/SamsungPage.jsx
+++ src/pages/SamsungPage.jsx
@@ -1,21 +1,16 @@
 import { useSelector } from "react-redux";
-import WatchAndPhoneCart from "../components/WatchAndPhoneCart";
-import { customStyle } from "../components/customStyle/CustomStyle";
+import PaginatedItems from "../components/pagination/PaginatedItems";
 
 const SamsungPage = () => {
   const data = useSelector((state) => state.cart.items);
 
-  return (
-    <div className="pt-44 grid__product gap-4 py-4 px-6">
-      {data.map((items, index) =>
-        items.name === "samsung" ? (
-          <WatchAndPhoneCart key={index} data={items} styling={customStyle} />
-        ) : (
-          ""
-        )
-      )}
-    </div>
-  );
+  let newData = [];
+  for (let key of data) {
+    if (key.name === "samsung") {
+      newData.push(key);
+    }
+  }
+  return <PaginatedItems itemsPerPage={4} data={newData} />;
 };
 
 export default SamsungPage;
