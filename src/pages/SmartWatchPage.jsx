88992200b2c295479729400478d192fc2ddd3109
Peyman Norozi <peyman.norozy@gmail.com>
2023-02-23 13:59:10 -0800

    add the pagination to  smartWatchPage component

---
+++ src/pages/SmartWatchPage.jsx
@@ -0,0 +1,16 @@
+import { useSelector } from "react-redux";
+import PaginatedItems from "../components/pagination/PaginatedItems";
+
+const SmartWatchPage = () => {
+  const data = useSelector((state) => state.cart.items);
+
+  let newData = [];
+  for (let key of data) {
+    if (key.name === "smartWatch") {
+      newData.push(key);
+    }
+  }
+  return <PaginatedItems itemsPerPage={4} data={newData} />;
+};
+
+export default SmartWatchPage;
