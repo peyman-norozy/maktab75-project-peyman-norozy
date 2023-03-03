2f0aa855b447ca2ae35afb55a796e48ce8184be6
Peyman Norozi <peyman.norozy@gmail.com>
2023-03-03 12:26:47 -0800

    add the error searching under the search bar

--- src/components/SearchProduct.jsx
+++ src/components/SearchProduct.jsx
@@ -1,7 +1,10 @@
 import { useSelector, useDispatch } from "react-redux";
 import { productActions } from "../store/cart-slice";
+import { useState } from "react";
 
 const SearchProduct = () => {
+  const [setSearchError, newSetSearchError] = useState(false);
+
   const data2 = useSelector((state) => state);
   const itemList = data2.cart.items;
   console.log(itemList);
@@ -16,38 +19,52 @@ const SearchProduct = () => {
     updatedList = updatedList.filter((item) => {
       return item.name.toLowerCase().indexOf(query.toLowerCase()) !== -1;
     });
+    console.log(updatedList);
+
+    if (updatedList.length === 0) {
+      newSetSearchError(true);
+    } else {
+      newSetSearchError(false);
+    }
 
     dispatch(productActions.addSearchItem(updatedList));
   };
 
   return (
-    <div className="max-w-md mx-auto mt-[20px]">
-      <div className="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-[#eee] overflow-hidden">
-        <div className="grid place-items-center h-full w-12 text-gray-300">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </div>
+    <>
+      <div className="max-w-md mx-auto mt-[20px]">
+        <div className="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-[#eee] overflow-hidden">
+          <div className="grid place-items-center h-full w-12 text-gray-300">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </div>
 
-        <input
-          className="h-full w-full outline-none text-sm text-gray-700 pr-2 bg-[#eee]"
-          type="search"
-          placeholder="جستوجو کنید..."
-          onChange={handleInputChange}
-        />
+          <input
+            className="h-full w-full outline-none text-sm text-gray-700 pr-2 bg-[#eee]"
+            type="search"
+            placeholder="جستوجو کنید..."
+            onChange={handleInputChange}
+          />
+        </div>
       </div>
-    </div>
+      {setSearchError && (
+        <p className="text-red-600 text-center mt-2 text-sm">
+          محصول مورد نظر یافت نشد
+        </p>
+      )}
+    </>
   );
 };
 
