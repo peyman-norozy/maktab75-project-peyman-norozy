8cfe99bac77f82e98acac0fd2a3450b6b29b6935
Peyman Norozi <peyman.norozy@gmail.com>
2023-03-20 06:47:39 -0700

    add the React.lazy() and Suspense to the Route page in App.js file

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
+import React, { Suspense } from "react";
 import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import ManagementMainPage from "./pages/ManagementMainPage";
-import CartMainPage from "./pages/CartMainPage";
-import Home from "./pages/Home";
-import IphonePage from "./pages/IphonePage";
-import ShiaomiPage from "./pages/ShiaomiPage";
-import SamsungPage from "./pages/SamsungPage";
-import SmartWatchPage from "./pages/SmartWatchPage";
-import Error from "./pages/ErrorPage";
-import ManagementPanel from "./pages/ManagementPanelPage";
-import ProductManagement from "./pages/ProductManagementPage";
-import InventoryManagement from "./pages/InventoryManagementPage";
-import SingleProductPage from "./pages/SingleProductPage";
-import FinalizeShoppingCart from "./pages/FinalizeShoppingCartPage";
-import InternetPayment from "./pages/InternetPaymentPage";
-import Orders from "./pages/OrdersPage";
-import SuccessfulPayment from "./pages/successfulPaymentPage";
-import PaymentFailure from "./pages/paymentFailurePage";
 import { productActions } from "./store/cart-slice";
 import { uiActions } from "./store/ui-slice";
 import { BASE_URL } from "./components/api/axios-constance/useHttp";
 import { products } from "./components/api/axios-constance/useHttp";
 import { nav } from "./components/api/axios-constance/useHttp";
+const ManagementMainPage = React.lazy(() =>
+  import("./pages/ManagementMainPage")
+);
+const CartMainPage = React.lazy(() => import("./pages/CartMainPage"));
+const Home = React.lazy(() => import("./pages/Home"));
+const IphonePage = React.lazy(() => import("./pages/IphonePage"));
+const ShiaomiPage = React.lazy(() => import("./pages/ShiaomiPage"));
+const SamsungPage = React.lazy(() => import("./pages/SamsungPage"));
+const SmartWatchPage = React.lazy(() => import("./pages/SmartWatchPage"));
+const Error = React.lazy(() => import("./pages/ErrorPage"));
+const ManagementPanel = React.lazy(() => import("./pages/ManagementPanelPage"));
+const ProductManagement = React.lazy(() =>
+  import("./pages/ProductManagementPage")
+);
+const InventoryManagement = React.lazy(() =>
+  import("./pages/InventoryManagementPage")
+);
+const SingleProductPage = React.lazy(() => import("./pages/SingleProductPage"));
+const FinalizeShoppingCart = React.lazy(() =>
+  import("./pages/FinalizeShoppingCartPage")
+);
+const InternetPayment = React.lazy(() => import("./pages/InternetPaymentPage"));
+const Orders = React.lazy(() => import("./pages/OrdersPage"));
+const SuccessfulPayment = React.lazy(() =>
+  import("./pages/successfulPaymentPage")
+);
+const PaymentFailure = React.lazy(() => import("./pages/paymentFailurePage"));
 
 function App() {
   const dispatch = useDispatch();
@@ -48,26 +59,28 @@ function App() {
 
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/management" element={<ManagementMainPage />} />
-        <Route path="/cart" element={<CartMainPage />} />
-        <Route path="/iphone" element={<IphonePage />} />
-        <Route path="/shiaomi" element={<ShiaomiPage />} />
-        <Route path="/samsung" element={<SamsungPage />} />
-        <Route path="/smartWatch" element={<SmartWatchPage />} />
-        <Route path="/singleProduct" element={<SingleProductPage />} />
-        <Route path="/finallyBasket" element={<FinalizeShoppingCart />} />
-        <Route path="/payment" element={<InternetPayment />} />
-        <Route path="/paymentSuccessfully" element={<SuccessfulPayment />} />
-        <Route path="/paymentFailure" element={<PaymentFailure />} />
-        <Route path="panel" element={<ManagementPanel />}>
-          <Route path="products" element={<ProductManagement />} />
-          <Route path="inventory" element={<InventoryManagement />} />
-          <Route path="orders" element={<Orders />} />
-        </Route>
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/management" element={<ManagementMainPage />} />
+          <Route path="/cart" element={<CartMainPage />} />
+          <Route path="/iphone" element={<IphonePage />} />
+          <Route path="/shiaomi" element={<ShiaomiPage />} />
+          <Route path="/samsung" element={<SamsungPage />} />
+          <Route path="/smartWatch" element={<SmartWatchPage />} />
+          <Route path="/singleProduct" element={<SingleProductPage />} />
+          <Route path="/finallyBasket" element={<FinalizeShoppingCart />} />
+          <Route path="/payment" element={<InternetPayment />} />
+          <Route path="/paymentSuccessfully" element={<SuccessfulPayment />} />
+          <Route path="/paymentFailure" element={<PaymentFailure />} />
+          <Route path="panel" element={<ManagementPanel />}>
+            <Route path="products" element={<ProductManagement />} />
+            <Route path="inventory" element={<InventoryManagement />} />
+            <Route path="orders" element={<Orders />} />
+          </Route>
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
